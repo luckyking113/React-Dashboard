bdf73698ab5e5f88c7e11226adbf3800f2ec638f
luckyking113 <king89494989@gmail.com>
2017-05-10 18:09:50 +0800

    finis homepage

--- src/page/welcome/child-welcome2.js
+++ src/page/welcome/child-welcome2.js
@@ -7,11 +7,11 @@ export class ChildWelcome2 extends React.Component {
             welcome2Sections: [
                 {childWelcome2Img: 'child-welcome-img1', childWelcome2label: 'CDB AGIPLAN'},
                 {childWelcome2Img: 'child-welcome-img2', childWelcome2label: 'CDB BANCO BMG'},
-                {childWelcome2Img: 'child-welcome-img3', childWelcome2label: 'TESOURO PREFIXADO'},
+                {childWelcome2Img: 'child-welcome-img3', childWelcome2label: 'TESOURO'},
                 {childWelcome2Img: 'child-welcome-img4', childWelcome2label: 'CDB BANCO BMG'},
                 {childWelcome2Img: 'child-welcome-img5', childWelcome2label: 'CDB BANCO BMG'},
-                {childWelcome2Img: 'child-welcome-img6', childWelcome2label: 'TESOURO PREFIXADO'},
-                {childWelcome2Img: 'child-welcome-img7', childWelcome2label: 'TESOURO PREFIXADO'},
+                {childWelcome2Img: 'child-welcome-img6', childWelcome2label: 'TESOURO'},
+                {childWelcome2Img: 'child-welcome-img7', childWelcome2label: 'TESOURO'},
                 {childWelcome2Img: 'child-welcome-img8', childWelcome2label: 'CDB AGIPLAN'},
             ]
         }
@@ -20,11 +20,15 @@ export class ChildWelcome2 extends React.Component {
     renderWelcome() {
         return _.map(this.state.welcome2Sections, (welcome2Section) => {
             return (
-                <div className = "col-md-3 ChildWelcome2"> 
-                    <div className={`childWelcome ${welcome2Section.childWelcome2Img}`} alt=""/> 
-                    <h3>{welcome2Section.childWelcome2label}</h3>
-                    <p>110% do CDI | Prazo 1 ano</p>
-                    <button className="btn btn-success">Investir</button>
+                <div className = "equal-height">
+                    <div className = "col-md-3 ChildWelcome2 equal-height-child"> 
+                        <div className = "childHeight">
+                            <div className={`childWelcome ${welcome2Section.childWelcome2Img}`} alt=""/> 
+                            <h3>{welcome2Section.childWelcome2label}</h3>
+                            <p>110% do CDI | Prazo 1 ano</p>
+                            <button className="btn btn-success">Investir</button>
+                        </div>
+                    </div>     
                 </div>     
             ); 
         });   
